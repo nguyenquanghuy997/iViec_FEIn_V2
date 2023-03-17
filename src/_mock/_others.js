0aa739be3c3a19ac66e661aa2d3028cf39bdc2df
Thang Tran <thangtran@MacBook-Pro-cua-Thang.local>
2023-03-17 08:59:08 +0700

    add-mock-data-kanban

---
+++ src/_mock/_others.js
@@ -0,0 +1,275 @@
+const _board = {
+  cards: [
+    {
+      id: 'deb02f04-9cf8-4f1e-97e0-2fbda84cc6b3',
+      name: 'Call with sales of HubSpot',
+      description:
+        'Duis condimentum lacus finibus felis pellentesque, ac auctor nibh fermentum. Duis sed dui ante. Phasellus id eros tincidunt, dictum lorem vitae, pellentesque sem. Aenean eu enim sit amet mauris rhoncus mollis. Sed enim turpis, porta a felis et, luctus faucibus nisi. Phasellus et metus fermentum, ultrices arcu aliquam, facilisis justo. Cras nunc nunc, elementum sed euismod ut, maximus eget nibh. Phasellus condimentum lorem neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce sagittis pharetra eleifend. Suspendisse potenti.',
+      assignee: [
+        {
+          id: '473d2720-341c-49bf-94ed-556999cf6ef7',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_2.jpg',
+          name: 'Lucian Obrien',
+        },
+      ],
+      due: [1656757942824, 1656844342824],
+      attachments: [],
+      comments: [
+        {
+          id: 'ee8d1d46-f28b-4701-a4fd-0566c0f2680b',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_1.jpg',
+          name: 'Jayvion Simon',
+          createdAt: '2022-06-26T10:32:22.824Z',
+          messageType: 'text',
+          message: 'Assumenda nam repudiandae rerum fugiat vel maxime.',
+        },
+        {
+          id: 'dc7a35fd-7318-4df0-a2cc-15a44cc3c3d3',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_2.jpg',
+          name: 'Lucian Obrien',
+          createdAt: '2022-06-25T09:32:22.824Z',
+          messageType: 'text',
+          message: 'Quis veniam aut saepe aliquid nulla.',
+        },
+        {
+          id: '3dfbba16-e0c1-4ecc-a5bb-6b1a31a3efe4',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_3.jpg',
+          name: 'Deja Brady',
+          createdAt: '2022-06-24T08:32:22.824Z',
+          messageType: 'text',
+          message: 'Reprehenderit ut voluptas sapiente ratione nostrum est.',
+        },
+        {
+          id: '60ba85d0-3b82-4182-8641-270e8bd6c67e',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_4.jpg',
+          name: 'Harrison Stein',
+          createdAt: '2022-06-23T07:32:22.824Z',
+          messageType: 'image',
+          message:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/feeds/feed_7.jpg',
+        },
+        {
+          id: '263af66a-f4f2-4de2-a302-0553ae10c692',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_5.jpg',
+          name: 'Reece Chung',
+          createdAt: '2022-06-22T06:32:22.824Z',
+          messageType: 'text',
+          message: 'Quo quia sit nihil nemo doloremque et.',
+        },
+        {
+          id: 'a3234c3d-fb98-41f3-8dca-6e7978a8962d',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_6.jpg',
+          name: 'Lainey Davidson',
+          createdAt: '2022-06-21T05:32:22.824Z',
+          messageType: 'image',
+          message:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/feeds/feed_9.jpg',
+        },
+        {
+          id: '42e90f9e-268d-4137-ac0c-1f87a8597433',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_7.jpg',
+          name: 'Cristopher Cardenas',
+          createdAt: '2022-06-20T04:32:22.824Z',
+          messageType: 'text',
+          message:
+            'Tempora officiis consequuntur architecto nostrum autem nam adipisci.',
+        },
+        {
+          id: '8f570916-524a-4abe-a4d4-ac9c76db2cf3',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_8.jpg',
+          name: 'Melanie Noble',
+          createdAt: '2022-06-19T03:32:22.824Z',
+          messageType: 'text',
+          message: 'Voluptas sunt magni adipisci praesentium saepe.',
+        },
+      ],
+      completed: true,
+    },
+    {
+      id: '98bf6e8b-becc-485b-9c3f-a7d09392c48d',
+      name: 'Interview for the Asis. Sales Manager',
+      description:
+        'We are looking for vue experience and of course node js strong knowledge',
+      assignee: [
+        {
+          id: '473d2720-341c-49bf-94ed-556999cf6ef7',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_2.jpg',
+          name: 'Deja Brady',
+        },
+        {
+          id: 'b8395203-887c-46f5-a85f-339b2d75c98b',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_3.jpg',
+          name: 'Harrison Stein',
+        },
+        {
+          id: '18e23ac9-c874-43e4-8163-2d37f15f3367',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_4.jpg',
+          name: 'Reece Chung',
+        },
+        {
+          id: 'a3be5485-03bf-47a6-b553-a9cf9f070ed8',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_5.jpg',
+          name: 'Lainey Davidson',
+        },
+        {
+          id: '048f6343-7a65-4873-a570-eb6ff4eb1ba3',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_6.jpg',
+          name: 'Cristopher Cardenas',
+        },
+      ],
+      due: [1656757942824, 1656844342824],
+      attachments: [
+        'https://minimal-assets-api-dev.vercel.app/assets/images/feeds/feed_2.jpg',
+      ],
+      comments: [],
+      completed: false,
+    },
+    {
+      id: '99fbc02c-de89-4be3-9515-f8bd12227d38',
+      name: 'Change the height of the top bar because it looks too chunky',
+      description:
+        'We nede to make it aggressive with pricing because it’s in their interest to acquire us',
+      assignee: [],
+      due: [null, null],
+      attachments: [],
+      comments: [],
+      completed: true,
+    },
+    {
+      id: 'ab9cebca-6cb4-4847-aa17-3b261b3dd0fb',
+      name: 'Integrate Stripe API',
+      description:
+        'We nede to make it aggresive with pricing because it’s in their interest to acquire us',
+      assignee: [
+        {
+          id: 'b8395203-887c-46f5-a85f-339b2d75c98b',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_3.jpg',
+          name: 'Melanie Noble',
+        },
+        {
+          id: 'a3be5485-03bf-47a6-b553-a9cf9f070ed8',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_6.jpg',
+          name: 'Chase Day',
+        },
+      ],
+      due: [null, null],
+      attachments: [
+        'https://minimal-assets-api-dev.vercel.app/assets/images/feeds/feed_4.jpg',
+      ],
+      comments: [],
+      completed: false,
+    },
+    {
+      id: 'ebf0d26a-78e5-414f-986f-003d8fcd3154',
+      name: 'Update the customer API for payments',
+      description:
+        'We need to make it aggresive with pricing because it’s in their interest to acquire us',
+      assignee: [
+        {
+          id: '473d2720-341c-49bf-94ed-556999cf6ef7',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_2.jpg',
+          name: 'Shawn Manning',
+        },
+      ],
+      due: [null, null],
+      attachments: [],
+      comments: [],
+      completed: true,
+    },
+    {
+      id: '9d98ce30-3c51-4de3-8537-7a4b663ee3af',
+      name: 'Release minimals DS',
+      description: 'Production',
+      assignee: [
+        {
+          id: '473d2720-341c-49bf-94ed-556999cf6ef7',
+          avatar:
+            'https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_2.jpg',
+          name: 'Soren Durham',
+        },
+      ],
+      due: [null, null],
+      attachments: [],
+      comments: [],
+      completed: true,
+    },
+  ],
+  columns: [
+    {
+      id: '8cd887ec-b3bc-11eb-8529-0242ac130003',
+      name: 'Backlog',
+      cardIds: [
+        'deb02f04-9cf8-4f1e-97e0-2fbda84cc6b3',
+        '98bf6e8b-becc-485b-9c3f-a7d09392c48d',
+        '99fbc02c-de89-4be3-9515-f8bd12227d38',
+      ],
+    },
+    {
+      id: '23008a1f-ad94-4771-b85c-3566755afab7',
+      name: 'Progress',
+      cardIds: [
+        'ab9cebca-6cb4-4847-aa17-3b261b3dd0fb',
+        'ebf0d26a-78e5-414f-986f-003d8fcd3154',
+      ],
+    },
+    {
+      id: '37a9a747-f732-4587-a866-88d51c037641',
+      name: 'Q&A',
+      cardIds: [],
+    },
+    {
+      id: '4ac3cd37-b3e1-466a-8e3b-d7d88f6f5d4f',
+      name: 'Production',
+      cardIds: ['9d98ce30-3c51-4de3-8537-7a4b663ee3af'],
+    },
+  ],
+  columnOrder: [
+    '8cd887ec-b3bc-11eb-8529-0242ac130003',
+    '23008a1f-ad94-4771-b85c-3566755afab7',
+    '37a9a747-f732-4587-a866-88d51c037641',
+    '4ac3cd37-b3e1-466a-8e3b-d7d88f6f5d4f',
+  ],
+}
+
+function objFromArray(array, key = 'id') {
+  return array.reduce((accumulator, current) => {
+    accumulator[current[key]] = current
+    return accumulator
+  }, {})
+}
+
+const cards = objFromArray(_board.cards)
+const columns = objFromArray(_board.columns)
+const { columnOrder } = _board
+
+export const board = {
+  cards,
+  columns,
+  columnOrder,
+}
+
+export const _contacts = [...Array(20)].map((_, index) => ({
+  id: index,
+  name: `Name - ${index}`,
+  avatar: `https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_${
+    index + 1
+  }.jpg`,
+  email: `email${index}@gmail.com`,
+}))
