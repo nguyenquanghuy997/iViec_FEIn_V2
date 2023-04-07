34461d92c5c1a5fde5558965c73fcefd208a09f1
HuongNC <huongnc8@fpt.com.vn>
2023-04-07 18:00:47 +0700

    fix recruitment form

--- src/sections/recruitment-form/information/Owner.js
+++ src/sections/recruitment-form/information/Owner.js
@@ -3,16 +3,14 @@ import {Box} from "@mui/material";
 import {useWatch} from "react-hook-form";
 
 import {RHFSelect} from "@/components/hook-form";
-import RHFMuiAutocomplete from "@/components/hook-form/RHFMuiAutocomplete";
 import DividerCard from "@/sections/recruitment-form/components/DividerCard";
 
-import {API_GET_USER_FROM_ORGANIZATION} from "@/routes/api";
+import {API_GET_LIST_USER_ORGANIZATION} from "@/routes/api";
 
 import {BoxInnerStyle} from "@/sections/recruitment-form/style";
 import {LabelStyle} from "@/components/hook-form/style";
 const Owner = () => {
     const organizationId = useWatch({name: 'organizationId'});
-
     return (
         <BoxInnerStyle>
             <DividerCard title="CÁN BỘ TUYỂN DỤNG VÀ HỘI ĐỒNG TUYỂN DỤNG"/>
@@ -25,7 +23,7 @@ const Owner = () => {
                 <Box sx={{mb: 2, width: '50%'}}>
                     <LabelStyle required>Cán bộ tuyển dụng</LabelStyle>
                     <RHFSelect
-                        remoteUrl={`${API_GET_USER_FROM_ORGANIZATION}?Id=${organizationId}`}
+                        remoteUrl={`${API_GET_LIST_USER_ORGANIZATION}?OrganizationId=${organizationId}`}
                         name="ownerId"
                         placeholder="Chọn 1 cán bộ"
                         fullWidth
@@ -33,27 +31,25 @@ const Owner = () => {
                     />
                 </Box>
                 <Box sx={{mb: 2}}>
-                    <RHFMuiAutocomplete
-                        remoteUrl={`${API_GET_USER_FROM_ORGANIZATION}?Id=${organizationId}`}
+                    <LabelStyle>Đồng phụ trách</LabelStyle>
+                    <RHFSelect
+                        remoteUrl={`${API_GET_LIST_USER_ORGANIZATION}?OrganizationId=${organizationId}`}
                         name="coOwnerIds"
-                        title="Đồng phụ tráchh"
                         placeholder="Chọn 1 hoặc nhiều cán bộ"
                         fullWidth
                         multiple
                         showAvatar
-                        disableCloseOnSelect
                     />
                 </Box>
                 <Box sx={{mb: 2}}>
-                    <RHFMuiAutocomplete
-                        remoteUrl={`${API_GET_USER_FROM_ORGANIZATION}?Id=${organizationId}`}
+                    <LabelStyle required>Thành viên hội đồng tuyển dụng</LabelStyle>
+                    <RHFSelect
+                        remoteUrl={`${API_GET_LIST_USER_ORGANIZATION}?OrganizationId=${organizationId}`}
                         name="recruitmentCouncilIds"
-                        title="Thành viên hội đồng tuyển dụng"
                         placeholder="Chọn 1 hoặc nhiều cán bộ"
                         fullWidth
                         multiple
                         showAvatar
-                        disableCloseOnSelect
                     />
                 </Box>
             </Box>
