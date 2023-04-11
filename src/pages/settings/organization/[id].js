2c73a96881b56890e24765995d9cb479204d96a4
HuongNC <huongnc8@fpt.com.vn>
2023-04-11 11:19:44 +0700

    fix rtk applicant

--- src/pages/settings/organization/[id].js
+++ src/pages/settings/organization/[id].js
@@ -1,15 +1,7 @@
-import React, {useEffect} from 'react'
 import SettingLayout from "@/layouts/setting";
 import { PERMISSION_PAGES } from "@/config";
 import Page from "@/components/Page";
 import OrganizationDetailContent from "@/sections/organizationdetail/OrganizationDetailContent";
-import {useRouter} from "next/router";
-import {
-    useGetAllApplicantUserOrganizationByIdQuery,
-    useGetListOrganizationWithChildQuery,
-    useGetOrganizationByIdQuery
-} from "@/sections/organization/override/OverrideOrganizationSlice";
-import LoadingScreen from "@/components/LoadingScreen";
 
 OrganizationDetail.getLayout = function getLayout(pageProps, page) {
     return (
@@ -20,33 +12,9 @@ OrganizationDetail.getLayout = function getLayout(pageProps, page) {
 };
 
 function OrganizationDetail() {
-
-    const router = useRouter();
-    const {query, isReady} = router;
-
-    const {data: organization = {}} = useGetOrganizationByIdQuery({
-        OrganizationId: query?.id
-    }, {skip: !query?.id});
-
-    const {data: {items: ListUser = []} = {}, isLoading: loadingUser} = useGetAllApplicantUserOrganizationByIdQuery({
-        OrganizationId: query?.id
-    }, {skip: !query?.id});
-
-    const {data: {items: ListOrganization = []} = {}} = useGetListOrganizationWithChildQuery();
-
-    useEffect(() => {
-        if (!isReady) return;
-    }, [isReady])
-
-    if (loadingUser) return <LoadingScreen />;
-
     return (
         <Page title="Thông tin chi tiết đơn vị">
-            <OrganizationDetailContent
-                organization={organization}
-                ListUser={ListUser}
-                ListOrganization={ListOrganization}
-            />
+            <OrganizationDetailContent />
         </Page>
     )
 }
