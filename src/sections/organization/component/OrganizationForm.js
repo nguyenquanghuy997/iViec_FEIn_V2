b417a87d14f72233ceeb6134174c434c328c8d85
chung <chungkv@fpt.com.vn>
2023-04-11 18:24:20 +0700

    ignore require form organization

--- src/sections/organization/component/OrganizationForm.js
+++ src/sections/organization/component/OrganizationForm.js
@@ -221,7 +221,7 @@ const OrganizationForm = ({isOpen, onClose, parentNode, actionType}) => {
                   />
                 </View>
                 <View>
-                  <Label required={true}>Email đơn vị</Label>
+                  <Label>Email đơn vị</Label>
                   <RHFTextField
                       name="email"
                       placeholder="Nhập email đơn vị"
@@ -230,7 +230,7 @@ const OrganizationForm = ({isOpen, onClose, parentNode, actionType}) => {
                   />
                 </View>
                 <View>
-                  <Label required={true}>Số điện thoại đơn vị</Label>
+                  <Label>Số điện thoại đơn vị</Label>
                   <RHFTextField
                       name="phoneNumber"
                       placeholder="Nhập SĐT đơn vị"
