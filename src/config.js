b8246cf3d396fa58de2697f6c4a6c241af6c1219
Thanh Thủy <thuybon1@gmail.com>
2023-04-17 11:20:56 +0700

    update file env

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 import { PATH_DASHBOARD } from "@/routes/paths";
 import { enUS, viVN } from "@mui/material/locale";
 
-export const DOMAIN_SERVER_API = process.env.NEXT_PUBLIC_DOMAIN_SERVER_API || "https://api.iviec.vn/api";
-// "http://103.176.149.158:5001/api";
+export const DOMAIN_SERVER_API = process.env.NEXT_PUBLIC_DOMAIN_SERVER_API;
 
 export const PATH_AFTER_LOGIN = PATH_DASHBOARD.dashboard;
-export const DOMAIN_OUTSIDE = "https://iviec.vn/";
+export const DOMAIN_OUTSIDE = process.env.NEXT_PUBLIC_DOMAIN_OUTSIDE;
 
 export const DASHBOARD_TABLE_HEIGHT = 500;
 
