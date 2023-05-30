20f0967a8501654574094880129222ba6cacd4fc
Le Minh Tuan <tuanlm@hybrid-technologies.vn>
2023-05-30 14:18:31 +0700

    fix bug https://dev.azure.com/FPTJobs/iVIEC%20v2/_workitems/edit/6378/

--- src/sections/applicant/items/DetailApplicantRight/NotificationBoard.js
+++ src/sections/applicant/items/DetailApplicantRight/NotificationBoard.js
@@ -144,7 +144,7 @@ const NotificationBoard = ({
             </React.Fragment>
           </div>
 
-          {isReview && (
+          {isReview ? (
             <Collapse
               in={open}
               timeout="auto"
@@ -219,6 +219,24 @@ const NotificationBoard = ({
 
               {children}
             </Collapse>
+          ) : (
+            !!children && (
+              <Collapse
+                in={open}
+                timeout="auto"
+                unmountOnExit
+                sx={{
+                  backgroundColor: theme.palette.common.bgrMaster,
+                  pb: 2,
+                  pt: 2,
+                  "& .MuiButtonBase-root:hover": {
+                    backgroundColor: theme.palette.common.bgrMaster,
+                  },
+                }}
+              >
+                {children}
+              </Collapse>
+            )
           )}
 
           {isReview && isOpenReview && (
