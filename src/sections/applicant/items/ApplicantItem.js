059d1ee3177425917d899f446d2388ddc7b4e677
Le Minh Tuan <tuanlm@hybrid-technologies.vn>
2023-05-30 14:57:00 +0700

    fix bug https://dev.azure.com/FPTJobs/iVIEC%20v2/_workitems/edit/6371/

--- src/sections/applicant/items/ApplicantItem.js
+++ src/sections/applicant/items/ApplicantItem.js
@@ -243,7 +243,7 @@ export const ApplicantItem = (props) => {
         render: (jobCats = []) => {
           return (
             jobCats
-              .map((cat) => cat.name)
+              .map((cat) => cat.jobCategoryName)
               .filter((i) => i)
               .join(", ") || "-"
           );
