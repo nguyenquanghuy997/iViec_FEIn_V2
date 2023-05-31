0517de4475baf3922c79bbf9e406d1c6b44e0fe8
Le Minh Tuan <tuanlm@hybrid-technologies.vn>
2023-05-31 16:33:59 +0700

    fix bug https://dev.azure.com/FPTJobs/iVIEC%20v2/_workitems/edit/6678/

--- src/sections/applicant/items/ApplicantCV.js
+++ src/sections/applicant/items/ApplicantCV.js
@@ -2,7 +2,7 @@ import NoCV from "@/assets/NoCV";
 import { Text, View } from "@/components/FlexStyled";
 import SvgIcon from "@/components/SvgIcon";
 import { getFileUrl } from "@/utils/helper";
-import { Box, Divider, Grid, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
@@ -83,22 +83,41 @@ export const ApplicantCV = ({ dataLog, pressUpload }) => {
                 </div>
               </>
             ) : (
-              <Box sx={{ minHeight: "1040px" }}>
-                <Box sx={{ textAlign: "center", marginTop: 13 }}>
-                  <NoCV />
-                </Box>
+              <View atCenter pt={92} style={{ minHeight: "1040px" }}>
+                <NoCV />
+
                 <Typography
                   sx={{
-                    mt: 2,
-                    textAlign: "center",
+                    mt: "12px",
                     fontSize: 14,
-                    fontWeight: 400,
+                    fontWeight: 500,
                     color: theme.palette.common.neutral400,
                   }}
                 >
                   {"Ứng viên chưa có CV cho tin tuyển dụng này."}
                 </Typography>
-              </Box>
+
+                <Button
+                  sx={{
+                    mt: "24px",
+                    border: "1px solid " + theme.palette.common.blue700,
+                    color: theme.palette.common.blue700,
+                    fontWeight: 600,
+                    padding: "5px 12px",
+                    textTransform: "inherit",
+                  }}
+                  startIcon={
+                    <SvgIcon>
+                      {
+                        '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_8688_60886)"> <path d="M3 14.25H15V9H16.5V15C16.5 15.1989 16.421 15.3897 16.2803 15.5303C16.1397 15.671 15.9489 15.75 15.75 15.75H2.25C2.05109 15.75 1.86032 15.671 1.71967 15.5303C1.57902 15.3897 1.5 15.1989 1.5 15V9H3V14.25ZM10.5 6.75V11.25H7.5V6.75H3.75L9 1.5L14.25 6.75H10.5Z" fill="#1976D2"/> </g> <defs> <clipPath id="clip0_8688_60886"> <rect width="18" height="18" fill="white"/> </clipPath> </defs> </svg>'
+                      }
+                    </SvgIcon>
+                  }
+                  onClick={pressUpload}
+                >
+                  Tải lên CV
+                </Button>
+              </View>
             )}
           </Grid>
         </Box>
