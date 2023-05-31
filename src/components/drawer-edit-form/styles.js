a4f7b8a0de6bfff515344aa785af11cc4c0c6fc4
Le Minh Tuan <tuanlm@hybrid-technologies.vn>
2023-05-31 13:57:55 +0700

    fix bug https://dev.azure.com/FPTJobs/iVIEC%20v2/_workitems/edit/6614/

--- src/components/drawer-edit-form/styles.js
+++ src/components/drawer-edit-form/styles.js
@@ -1,36 +1,42 @@
-export const drawerPaperStyle = ({ palette, width = 800, contentStyles = {} }) => ({
-  height: '100vh',
-  width: width + 'px',
-  maxWidth: '95%',
-  boxShadow: '-3px 0px 5px rgba(9, 30, 66, 0.2), 0px 0px 1px rgba(9, 30, 66, 0.3)',
-  overflow: 'hidden',
-  '.edit-header': {
-    padding: '15px 24px',
-    minHeight: '68px',
+export const drawerPaperStyle = ({
+  palette,
+  width = 800,
+  contentStyles = {},
+}) => ({
+  height: "100vh",
+  width: width + "px",
+  maxWidth: "95%",
+  background: palette.background.paper,
+  boxShadow:
+    "-3px 0px 5px rgba(9, 30, 66, 0.2), 0px 0px 1px rgba(9, 30, 66, 0.3)",
+  overflow: "hidden",
+  ".edit-header": {
+    padding: "15px 24px",
+    minHeight: "68px",
     borderBottom: "1px solid " + palette.text.border,
   },
-  '> form': {
-    display: 'flex',
-    flexDirection: 'column',
-    height: 'calc(100vh)',
+  "> form": {
+    display: "flex",
+    flexDirection: "column",
+    height: "calc(100vh)",
   },
-  '.role-actions-table': {
-    '.MuiTableHead-root': {
-      'tr th': {
+  ".role-actions-table": {
+    ".MuiTableHead-root": {
+      "tr th": {
         background: palette.background.paper,
       },
     },
   },
-  '.edit-container': {
+  ".edit-container": {
     flex: 1,
-    overflow: 'auto',
-    padding: '24px',
+    overflow: "auto",
+    padding: "24px",
     ...contentStyles,
   },
-  '.edit-footer': {
+  ".edit-footer": {
     background: palette.common.white,
     padding: "14px 24px",
-    minHeight: '68px',
-    borderTop: '1px solid ' + palette.text.border,
+    minHeight: "68px",
+    borderTop: "1px solid " + palette.text.border,
   },
 });
