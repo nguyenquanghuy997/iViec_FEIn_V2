d49dbc630cd51466dc9960edaaad4bf2114016ac
datdt44 <datdt44@fpt.com.vn>
2023-06-02 11:06:20 +0700

    bugfix/6666

--- src/sections/kanban/TaskCard.js
+++ src/sections/kanban/TaskCard.js
@@ -2,26 +2,26 @@ import {
   useLazyGetAllFilterApplicantQuery,
   useLazyGetApplicantCurrentStateWithRecruitmentStatesQuery,
 } from "../applicant";
-import { RejectApplicantModal } from "../applicant/modals/RejectApplicantModal";
-import { useLazyGetCompanyInfoQuery } from "../companyinfor/companyInforSlice";
-import { useGetBookingCalendarsByApplicantRecruitmentPipelineStateQuery } from "../interview";
-import { FormCalendar } from "../interview/components/FormCalendar";
+import {RejectApplicantModal} from "../applicant/modals/RejectApplicantModal";
+import {useLazyGetCompanyInfoQuery} from "../companyinfor/companyInforSlice";
+import {useGetBookingCalendarsByApplicantRecruitmentPipelineStateQuery} from "../interview";
+import {FormCalendar} from "../interview/components/FormCalendar";
 import ExaminerModal from "../recruitment/modals/ExaminerModal";
-import { CircleLineIcon, EditIcon } from "@/assets/ActionIcon";
-import { AvatarDS, ButtonDS } from "@/components/DesignSystem";
-import { LightTooltip } from "@/components/DesignSystem/TooltipHtml";
+import {CircleLineIcon, EditIcon} from "@/assets/ActionIcon";
+import {AvatarDS, ButtonDS} from "@/components/DesignSystem";
+import {LightTooltip} from "@/components/DesignSystem/TooltipHtml";
 import Iconify from "@/components/Iconify";
 import SvgIcon from "@/components/SvgIcon";
-import { ApplicantInterviewState } from "@/config";
-import { PATH_DASHBOARD } from "@/routes/paths";
+import {ApplicantInterviewState} from "@/config";
+import {PATH_DASHBOARD} from "@/routes/paths";
 import ApplicantSendOfferModal from "@/sections/applicant/modals/ApplicantSendOfferModal";
-import { srcImage } from "@/utils/enum";
-import { fDate, fTime } from "@/utils/formatTime";
+import {srcImage} from "@/utils/enum";
+import {fDate, fTime} from "@/utils/formatTime";
 import {
   Box,
   Button,
   ButtonGroup,
-  ClickAwayListener,
+  ClickAwayListener, Divider,
   Grid,
   MenuItem,
   MenuList,
@@ -30,16 +30,15 @@ import {
   Typography,
   useTheme,
 } from "@mui/material";
-import { Divider } from "antd";
 import moment from "moment";
-import { useRouter } from "next/router";
-import { useSnackbar } from "notistack";
+import {useRouter} from "next/router";
+import {useSnackbar} from "notistack";
 import PropTypes from "prop-types";
-import { memo, useEffect, useState } from "react";
-import { Draggable } from "react-beautiful-dnd";
+import {memo, useEffect, useState} from "react";
+import {Draggable} from "react-beautiful-dnd";
 
 function Item(props) {
-  const { sx, ...other } = props;
+  const {sx, ...other} = props;
   return (
     <Box
       sx={{
@@ -54,7 +53,7 @@ function Item(props) {
 }
 
 function Baseitem(props) {
-  const { item } = props;
+  const {item} = props;
   const router = useRouter();
   return (
     <Grid
@@ -97,7 +96,7 @@ function Baseitem(props) {
                 },
               },
               undefined,
-              { shallow: true }
+              {shallow: true}
             )
           }
         >
@@ -112,7 +111,7 @@ function Baseitem(props) {
 }
 
 function ExaminationItem(props) {
-  const { item } = props;
+  const {item} = props;
   const [showExam, setShowExam] = useState(false);
 
   // const { data: ExamResult } = useGetExamApplicantWithResultQuery(
@@ -124,7 +123,7 @@ function ExaminationItem(props) {
   // );
   return (
     <div>
-      <Baseitem item={item} />
+      <Baseitem item={item}/>
       <Box
         sx={{
           // backgroundColor: item?.processStatus == 4 ? "#E8F5E9" : "#FFEBEE",
@@ -181,33 +180,33 @@ function ExaminationItem(props) {
         </Stack>
       </Box>
       {showExam && (
-        <ExaminerModal show={showExam} onClose={() => setShowExam(false)} />
+        <ExaminerModal show={showExam} onClose={() => setShowExam(false)}/>
       )}
     </div>
   );
 }
 
 function InterviewItem(props) {
-  const { item } = props;
+  const {item} = props;
   const [open, setOpen] = useState(false);
   const [data, setData] = useState();
   const handleClick = (item) => {
     setOpen(true);
     setData(item);
   };
-  const { data: { items: interview } = { items: [] } } =
+  const {data: {items: interview} = {items: []}} =
     useGetBookingCalendarsByApplicantRecruitmentPipelineStateQuery(
       {
         ApplicantId: item?.applicantId,
         RecruitmentPipelineStateId: item?.recruitmentPipelineStateId,
       },
-      { skip: !item }
+      {skip: !item}
     );
   const lastInterview = interview[interview.length - 1];
   var timeEnd = moment(lastInterview?.interviewTime).add(
     lastInterview?.interviewDuration
   );
-  const { enqueueSnackbar } = useSnackbar();
+  const {enqueueSnackbar} = useSnackbar();
   const [getCompanyInfo] = useLazyGetCompanyInfoQuery();
 
   const getLink = async (id) => {
@@ -226,8 +225,8 @@ function InterviewItem(props) {
 
   return (
     <div>
-      <Baseitem item={item} />
-      <Box style={{ margin: "12px 0px 0px 0px", borderRadius: "4px" }}>
+      <Baseitem item={item}/>
+      <Box style={{margin: "12px 0px 0px 0px", borderRadius: "4px"}}>
         {/* Lịch sử cuộc pv */}
         {interview &&
           interview?.map((item, index) => {
@@ -272,11 +271,11 @@ function InterviewItem(props) {
           })}
         <Box>
           {lastInterview?.applicantInterviewState ===
-            ApplicantInterviewState.PENDING ||
+          ApplicantInterviewState.PENDING ||
           lastInterview?.applicantInterviewState ===
-            ApplicantInterviewState.CONFIRMED ||
+          ApplicantInterviewState.CONFIRMED ||
           lastInterview?.applicantInterviewState ===
-            ApplicantInterviewState.INTERVIEWING ? (
+          ApplicantInterviewState.INTERVIEWING ? (
             <Box
               sx={{
                 background: "#4CAF50",
@@ -311,10 +310,10 @@ function InterviewItem(props) {
                   >
                     {lastInterview?.interviewTime
                       ? fDate(lastInterview?.interviewTime) +
-                        " " +
-                        fTime(lastInterview?.interviewTime) +
-                        " - " +
-                        fTime(timeEnd)
+                      " " +
+                      fTime(lastInterview?.interviewTime) +
+                      " - " +
+                      fTime(timeEnd)
                       : ""}
                   </Typography>
                 </Box>
@@ -341,6 +340,7 @@ function InterviewItem(props) {
                 <Box>
                   <Typography
                     sx={{
+                      display: 'inline-block',
                       fontSize: "12px",
                       color: "#5C6A82",
                       fontWeight: "500",
@@ -349,7 +349,7 @@ function InterviewItem(props) {
                       textOverflow: "ellipsis",
                     }}
                   >
-                    {`${window.location.origin}/phong-van.html...`}
+                    {`${window.location.origin}/phong-van.html234234234234234234234324234`}
                   </Typography>
                 </Box>
                 <Iconify
@@ -363,8 +363,8 @@ function InterviewItem(props) {
             </Box>
           ) : (
             <>
-              <Divider style={{ margin: 0 }} />
-              <Box style={{ padding: "12px 12px" }}>
+              <Divider style={{margin: 0}}/>
+              <Box style={{padding: "12px 12px"}}>
                 <ButtonDS
                   tittle={"Đặt lịch phỏng vấn"}
                   type="submit"
@@ -406,13 +406,13 @@ function InterviewItem(props) {
 
 function ResultItem(props) {
   const [isOpenSendOffer, setIsOpenSendOffer] = useState(false);
-  const { item } = props;
+  const {item} = props;
   return (
     <div>
-      <Baseitem item={item} />
-      <Box style={{ margin: "12px 0px 0px 0px", borderRadius: "4px" }}>
-        <Divider style={{ margin: "0px" }} />
-        <Box style={{ padding: "12px 12px " }}>
+      <Baseitem item={item}/>
+      <Box style={{margin: "12px 0px 0px 0px", borderRadius: "4px"}}>
+        <Divider style={{margin: "0px"}}/>
+        <Box style={{padding: "12px 12px "}}>
           <Box>
             <Box
               sx={{
@@ -422,7 +422,7 @@ function ResultItem(props) {
             >
               <ButtonGroup
                 fullWidth={true}
-                style={{ border: "1px solid #E7E9ED" }}
+                style={{border: "1px solid #E7E9ED"}}
               >
                 <Button
                   type="submit"
@@ -527,7 +527,9 @@ function ResultItem(props) {
             )}
             {item.pipelineStateResultType == 0 && (
               <ButtonDS
-                onClick={() => { setIsOpenSendOffer(true) }}
+                onClick={() => {
+                  setIsOpenSendOffer(true)
+                }}
                 tittle={"Gửi thư mời nhận việc"}
                 type="submit"
                 sx={{
@@ -563,13 +565,13 @@ function ResultItem(props) {
 }
 
 function OfferItem(props) {
-  const { item } = props;
+  const {item} = props;
   return (
     <div>
-      <Baseitem item={item} />
+      <Baseitem item={item}/>
       {item.offerStateResultType === 0 && (
-        <Box sx={{ display: "flex", pt: 1, borderRadius: "4px" }}>
-          <Item sx={{ flexShrink: 1 }}>
+        <Box sx={{display: "flex", pt: 1, borderRadius: "4px"}}>
+          <Item sx={{flexShrink: 1}}>
             <Iconify
               icon={"ri:mail-check-fill"}
               width={20}
@@ -577,10 +579,10 @@ function OfferItem(props) {
               color="#172B4D"
             />
           </Item>
-          <Item sx={{ width: "100%", color: "#172B4D" }}>
+          <Item sx={{width: "100%", color: "#172B4D"}}>
             Đã tạo thư mời nhận việc
           </Item>
-          <Item sx={{ flexShrink: 0 }}>
+          <Item sx={{flexShrink: 0}}>
             <Iconify
               icon={"fluent-mdl2:circle-half-full"}
               width={20}
@@ -591,8 +593,8 @@ function OfferItem(props) {
         </Box>
       )}
       {item.offerStateResultType === 1 && (
-        <Box sx={{ display: "flex", pt: 1, borderRadius: "4px" }}>
-          <Item sx={{ flexShrink: 1 }}>
+        <Box sx={{display: "flex", pt: 1, borderRadius: "4px"}}>
+          <Item sx={{flexShrink: 1}}>
             <Iconify
               icon={"ic:round-mark-email-unread"}
               width={20}
@@ -600,10 +602,10 @@ function OfferItem(props) {
               color="#F77A0C"
             />
           </Item>
-          <Item sx={{ width: "100%", color: "#F77A0C" }}>
+          <Item sx={{width: "100%", color: "#F77A0C"}}>
             Đã gửi, chờ phản hồi
           </Item>
-          <Item sx={{ flexShrink: 0 }}>
+          <Item sx={{flexShrink: 0}}>
             <Iconify
               icon={"fluent-mdl2:circle-half-full"}
               width={20}
@@ -614,8 +616,8 @@ function OfferItem(props) {
         </Box>
       )}
       {item.offerStateResultType === 2 && (
-        <Box sx={{ display: "flex", pt: 1, borderRadius: "4px" }}>
-          <Item sx={{ flexShrink: 1 }}>
+        <Box sx={{display: "flex", pt: 1, borderRadius: "4px"}}>
+          <Item sx={{flexShrink: 1}}>
             <Iconify
               icon={"material-symbols:check-circle"}
               width={20}
@@ -623,8 +625,8 @@ function OfferItem(props) {
               color="#388E3C"
             />
           </Item>
-          <Item sx={{ width: "100%", color: "#388E3C" }}>Đồng ý nhận việc</Item>
-          <Item sx={{ flexShrink: 0 }}>
+          <Item sx={{width: "100%", color: "#388E3C"}}>Đồng ý nhận việc</Item>
+          <Item sx={{flexShrink: 0}}>
             <Iconify
               icon={"fluent-mdl2:circle-half-full"}
               width={20}
@@ -635,8 +637,8 @@ function OfferItem(props) {
         </Box>
       )}
       {item.offerStateResultType === 3 && (
-        <Box sx={{ display: "flex", pt: 1, borderRadius: "4px" }}>
-          <Item sx={{ flexShrink: 1 }}>
+        <Box sx={{display: "flex", pt: 1, borderRadius: "4px"}}>
+          <Item sx={{flexShrink: 1}}>
             <Iconify
               icon={"mdi:alpha-x-circle"}
               width={20}
@@ -644,10 +646,10 @@ function OfferItem(props) {
               color="#D32F2F"
             />
           </Item>
-          <Item sx={{ width: "100%", color: "#D32F2F" }}>
+          <Item sx={{width: "100%", color: "#D32F2F"}}>
             Từ chối nhận việc
           </Item>
-          <Item sx={{ flexShrink: 0 }}>
+          <Item sx={{flexShrink: 0}}>
             <Iconify
               icon={"fluent-mdl2:circle-half-full"}
               width={20}
@@ -661,10 +663,10 @@ function OfferItem(props) {
   );
 }
 
-function TaskCard({ item, index, pipelineStateType }) {
+function TaskCard({item, index, pipelineStateType}) {
   const theme = useTheme();
   const router = useRouter();
-  const { enqueueSnackbar } = useSnackbar();
+  const {enqueueSnackbar} = useSnackbar();
   const [openGroup, setOpenGroup] = useState(false);
 
   const [getApplicant] = useLazyGetAllFilterApplicantQuery();
@@ -706,7 +708,7 @@ function TaskCard({ item, index, pipelineStateType }) {
         },
       },
       undefined,
-      { shallow: true }
+      {shallow: true}
     );
   };
 
@@ -792,12 +794,21 @@ function TaskCard({ item, index, pipelineStateType }) {
                           </svg>
                           `}
                     </SvgIcon>
-                    <span style={{ marginLeft: "8px" }}>
+                    <span style={{marginLeft: "8px"}}>
                       {fDate(item.createdTime)}
                     </span>
                   </Typography>
                 </Box>
                 <LightTooltip
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    padding: '0px',
+                    position: 'relative',
+                    width: '200px',
+                    height: '132px',
+                  }}
                   placement="bottom-start"
                   onClose={handleCloseGroup}
                   disableFocusListener
@@ -832,18 +843,40 @@ function TaskCard({ item, index, pipelineStateType }) {
                         <MenuItem
                           onClick={pressEdit}
                           sx={{
-                            width: "200px",
-                            height: "30px",
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            padding: '10px 12px 10px 16px',
+                            gap: '12px',
+                            width: '200px',
+                            height: '44px',
+                            background: '#FDFDFD',
+                            boxShadow: 'inset',
+                            // 0px -1px 0px #E7E9ED;
                           }}
                         >
-                          <EditIcon sx={{ mr: "12px" }} />
+                          <EditIcon sx={{mr: "12px"}}/>
                           <Typography ml={"12px"} variant={"textSize13600"}>
                             Chỉnh sửa
                           </Typography>
                         </MenuItem>
-                        <Divider />
-                        <MenuItem onClick={pressDelete}>
-                          <CircleLineIcon sx={{ mr: "12px" }} />
+                        <Divider sx={{
+                          margin: '0 !important'
+                        }}/>
+                        <MenuItem
+                          onClick={pressDelete}
+                          sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            padding: '10px 12px 10px 16px',
+                            gap: '12px',
+                            width: '200px',
+                            height: '44px',
+                            background: '#FDFDFD',
+                            boxShadow: 'inset',
+                          }}>
+                          <CircleLineIcon sx={{mr: "12px"}}/>
                           <Typography
                             color={theme.palette.text.warning}
                             ml={"12px"}
@@ -859,8 +892,16 @@ function TaskCard({ item, index, pipelineStateType }) {
                   <Button
                     size="small"
                     aria-haspopup="menu"
-                    style={{ minWidth: 0, padding: 0 }}
+                    style={{minWidth: 0, padding: 0}}
                     onClick={handleOpenGroup}
+                    sx={{
+                      ':hover': {
+                        background: '#EFF3F6',
+                      },
+                      borderRadius: '6px',
+                      width: '28px',
+                      height: '28px',
+                    }}
                   >
                     <Iconify
                       icon={"ph:dots-three-bold"}
@@ -872,13 +913,13 @@ function TaskCard({ item, index, pipelineStateType }) {
                 </LightTooltip>
               </Box>
 
-              <Box sx={{ cursor: "pointer" }}>
-                <Stack sx={{ borderRadius: "8px", background: "#FDFDFD" }}>
-                  {pipelineStateType === 0 && <Baseitem item={item} />}
-                  {pipelineStateType === 1 && <ExaminationItem item={item} />}
-                  {pipelineStateType === 2 && <InterviewItem item={item} />}
-                  {pipelineStateType === 3 && <ResultItem item={item} />}
-                  {pipelineStateType === 4 && <OfferItem item={item} />}
+              <Box sx={{cursor: "pointer"}}>
+                <Stack sx={{borderRadius: "8px", background: "#FDFDFD"}}>
+                  {pipelineStateType === 0 && <Baseitem item={item}/>}
+                  {pipelineStateType === 1 && <ExaminationItem item={item}/>}
+                  {pipelineStateType === 2 && <InterviewItem item={item}/>}
+                  {pipelineStateType === 3 && <ResultItem item={item}/>}
+                  {pipelineStateType === 4 && <OfferItem item={item}/>}
 
                   <Box
                     display="Grid"
