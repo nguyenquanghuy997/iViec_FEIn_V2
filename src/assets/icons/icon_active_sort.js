effb91e38719d1f51b65bd84ee22365fa0e4b784
DuyetNN <nguyennhatduyet.dev@gmail.com>
2023-06-03 11:45:13 +0700

    6756

---
+++ src/assets/icons/icon_active_sort.js
@@ -0,0 +1,9 @@
+import { SvgIcon } from "@mui/material";
+
+export default function IconActiveSort(props) {
+  return (
+    <SvgIcon viewBox="0 0 18 18" fontSize="small" {...props}>
+      <path d="M7 10.172L16.192 0.979004L17.607 2.393L7 13L0.636002 6.636L2.05 5.222L7 10.172Z" fill="#1976D2"/>
+    </SvgIcon>
+  );
+}
