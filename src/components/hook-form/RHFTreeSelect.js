32c9a73b282688b501531f5cb474014a384112ee
chungkv <khuatchung1012@gmail.com>
2023-06-04 15:49:13 +0700

    fixbug ignore disable when not found email

--- src/components/hook-form/RHFTreeSelect.js
+++ src/components/hook-form/RHFTreeSelect.js
@@ -1,47 +1,50 @@
-import {Stack} from "@mui/material";
-import {Controller, useFormContext} from "react-hook-form";
-import {LabelStyle,} from '@/components/hook-form/style';
+import { Stack } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
+import { LabelStyle, } from '@/components/hook-form/style';
 import MuiTreeSelect from "@/components/form/MuiTreeSelect";
 import HelperText from "@/components/BaseComponents/HelperText";
 
-function RHFTreeSelect({name, onChange, ...props}) {
-  const {control} = useFormContext();
-  const {options, isRequired, title, ...other} = props;
+function RHFTreeSelect({ name, onChange, ...props }) {
+  const { control } = useFormContext();
+  const { options, isRequired, title, disabled, ...other } = props;
   const handleDelete = (field, valueDelete) => {
     const newOptions = field.value.filter(item => item !== valueDelete);
     field.onChange(newOptions);
   };
 
   return (
-      <Controller
-          name={name}
-          control={control}
-          render={({field, fieldState: {error}}) => {
-            const {onChange: onFieldChange, ...otherField} = field;
-            return (
-                <>
-                  <Stack direction="column">
-                    {title && <LabelStyle required={isRequired}>{title}</LabelStyle>}
-                    <MuiTreeSelect
-                        name={name}
-                        onChange={e => {
-                          if (onChange) {
-                            onChange(e);
-                          }
-                          onFieldChange(e);
-                        }}
-                        {...otherField}
-                        error={!!error}
-                        options={options}
-                        onDelete={(item) => handleDelete(field, item)}
-                        {...other}
-                    />
-                    <HelperText errorText={error?.message}/>
-                  </Stack>
-                </>
-            )
-          }}
-      />
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => {
+        const { onChange: onFieldChange, ...otherField } = field;
+        return (
+          <>
+            <Stack direction="column">
+              {title && <LabelStyle required={isRequired}>{title}</LabelStyle>}
+              <MuiTreeSelect
+                name={name}
+                onChange={e => {
+                  if (onChange) {
+                    onChange(e);
+                  }
+                  onFieldChange(e);
+                }}
+                {...otherField}
+                error={!!error}
+                options={options}
+                onDelete={(item) => {
+                  if (!disabled)
+                    handleDelete(field, item)
+                }}
+                {...other}
+              />
+              <HelperText errorText={error?.message} />
+            </Stack>
+          </>
+        )
+      }}
+    />
   );
 }
 
